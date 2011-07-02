00336c8bd0700fd80d6eaa0d2763d736d1043639
Michael Pogoda <mpogoda@lavabit.com>
2011-07-02 19:33:34 +0300

    Finished with red-black tree

--- print_tree.rb
+++ print_tree.rb
@@ -4,29 +4,60 @@
 require 'graphviz'
 require 'rbtree'
 
-class RedBlackTree
-  class RedBlackTreeNode
-    def mnodes g
-      root = g.add_node "#{@data[:key]}"
-      root[:color] = :red if @red
-      left = if @left.nil?: g.add_node "nill#{@data[:key]}", :shape => :point
-             else           @left.mnodes g
-             end
-      g.add_edge root, left
+class TemplateTreeNode
+  def mnodes(g, nodes)
+    root = g.add_node "#{@key.to_s}", :style => :filled, :fillcolor => :black, :fontcolor => :white, :penwidth => 0
 
-      right = if @right.nil?: g.add_node "nilr#{@data[:key]}", :shape => :point
-              else            @right.mnodes g
-              end
-      g.add_edge root, right
-
-      root
+    if self == nodes[:node] then
+      root[:penwidth] = 2
+      root[:color] = :chartreuse
+    elsif self == nodes[:father] then
+      root[:penwidth] = 2
+      root[:color] = :blue
+    elsif self == nodes[:uncle] then
+      root[:penwidth] = 2
+      root[:color] = :green
+    elsif self == nodes[:grandpa] then
+      root[:penwidth] = 2
+      root[:color] = :cyan
+    elsif self == nodes[:sibling] then
+      root[:penwidth] = 2
+      root[:color] = :darkgoldenrod
+    elsif self == nodes[:cousin1] then
+      root[:penwidth] = 2
+      root[:color] = :deeppink
+    elsif self == nodes[:cousin2] then
+      root[:penwidth] = 2
+      root[:color] = :gold
     end
-  end
 
-  def print_tree(filename)
-    GraphViz.new(:G, :type => :graph, :truecolor => true) { |g|
-      @root.mnodes g
+    left  = if @left then  @left.mnodes g, nodes
+            else           g.add_node "nill#{@key.to_s}", :shape => :point
+            end
+    g.add_edge root, left, :decorate => true
+
+    right = if @right then @right.mnodes g, nodes
+            else           g.add_node "nilr#{@key.to_s}", :shape => :point
+            end
+    g.add_edge root, right, :decorate => true
+
+    root
+  end
+end
+
+class TemplateTree
+  def print_tree(filename, nodes = {})
+    GraphViz.new(:G, :type => :graph, :fontname => "Monaco", :dpi => 120) {|g|
+      @root.mnodes g, nodes
       g.output :png => filename + ".png"
     }
+  end
+end
+
+class RedBlackTreeNode < TemplateTreeNode
+  def mnodes(g, nodes = {})
+    root = super g, nodes
+    root[:fillcolor] = :red if @red
+    root
   end
 end
